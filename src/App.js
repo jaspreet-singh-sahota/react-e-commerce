708f039925f5116fae6a281b402571cfba688bf8
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-09-04 15:37:06 +0530

    Add homepage bilder plate

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import HomePage from './homepage.component';
+
 import './App.css';
 
-function App() {
-  return (
-    <div>
-     hello world
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className='App'>
+        <HomePage />
+      </div>
+    );
+  }
 }
+
 
 export default App;
