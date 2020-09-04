cd4e22bc8f8b7a40cd41a8f1cf0e384223d5a64c
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-09-04 17:41:47 +0530

    Add pages folder

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import HomePage from './homepage';
 
-import './App.css';
+import HomePage from './pages/homepage/homepage';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className='App'>
-        <HomePage />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
+
 
 
 export default App;
