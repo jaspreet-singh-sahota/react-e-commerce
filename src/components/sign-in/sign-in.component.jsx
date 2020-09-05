27d5e054ffa58129d5593f0b9c7e95f9dca120ab
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-09-06 01:10:59 +0530

    Add form boiler-plate

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+import './sign-in.styles.scss'
+
+class SignIn extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
+
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
