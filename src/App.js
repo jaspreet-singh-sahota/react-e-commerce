24c1ca1dbe31eba3f7cddd3cba48d4f20e34a71b
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-09-05 20:36:30 +0530

    Add links

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Header from "./components/header/header.component"
 import { ShopPage } from "./pages/shoppage/shop.page";
+import './App.css'
 
 import HomePage from './pages/homepage/homepage';
 
 function App() {
   return (
     <div className="container">
+      <Header/>
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
