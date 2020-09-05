4fd986cb457022aac8d61e9ced060e44e5e685a8
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-09-06 02:53:05 +0530

    Add form component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,18 +1,58 @@
 import React from "react"
 import './sign-in.styles.scss'
+import FormInput from '../form-input/form-input.component';
 
 class SignIn extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
+
     this.state = {
       email: '',
       password: ''
-    }
+    };
   }
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    this.setState({ email: '', password: '' });
+  };
+
+  handleChange = event => {
+    const { value, name } = event.target;
+    console.log(value, name)
+
+    this.setState({ [name]: value });
+  };
 
   render() {
     return (
-      <div></div>
-    )
+      <div className='sign-in'>
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            name='email'
+            type='email'
+            handleChange={this.handleChange}
+            value={this.state.email}
+            label='email'
+            required
+          />
+          <FormInput
+            name='password'
+            type='password'
+            value={this.state.password}
+            handleChange={this.handleChange}
+            label='password'
+            required
+          />
+          <button type='submit'> Sign in </button>
+        </form>
+      </div>
+    );
   }
 }
+
+export default SignIn;
--- src/pages/sign-in-and-sing-up/sign-in-and-sing-up.component.jsx
+++ src/pages/sign-in-and-sing-up/sign-in-and-sing-up.component.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import './sign-in-and-sing-up.styles.scss'
+import SignIn from '../../components/sign-in/sign-in.component'
 
 const SignInAndSingUpPage = () => (
-  <div className="sign-in-and-sing-up">SIGN IN</div>
+  <div className="sign-in-and-sing-up"><SignIn /></div>
 )
 
 export default SignInAndSingUpPage
