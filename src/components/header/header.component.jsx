80f5353c1d3884592e5a0d81c4c787a21984e29e
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-09-06 18:06:32 +0530

    Add sing-out functionality

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 import './header.styles.scss';
 import { ReactComponent as Logo } from "../../assets/crown.svg";
+import { auth } from "../../firebase/firebase.utils";
 
-const Header = () => (
+const Header = ({currentUser}) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
@@ -11,6 +12,11 @@ const Header = () => (
     <div className="options">
       <Link className="option" to="/shop">Shop</Link>
       <Link className="option" to="/contact">Contact</Link>
+      {
+        currentUser?
+        <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div> :
+        <Link className='option' to='/sign-in'>SIGN IN</Link>
+      }
     </div>
   </div>
 )
