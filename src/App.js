ed266d331fa548e578959826bb4f23d5f85a366b
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-09-06 17:45:31 +0530

    Add authebntication for sign-in

--- src/App.js
+++ src/App.js
@@ -4,20 +4,44 @@ import Header from "./components/header/header.component"
 import { ShopPage } from "./pages/shoppage/shop.page";
 import SignInAndSingUpPage from './pages/sign-in-and-sing-up/sign-in-and-sing-up.component'
 import './App.css'
+import { auth } from "./firebase/firebase.utils";
 
 import HomePage from './pages/homepage/homepage';
 
-function App() {
-  return (
-    <div className="container">
-      <Header/>
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route path='/sign-in' component={SignInAndSingUpPage} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({currentUser: user})
+      console.log(user)
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth()
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Header />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/sign-in' component={SignInAndSingUpPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
