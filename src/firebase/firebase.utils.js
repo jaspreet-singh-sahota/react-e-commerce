b05344616f31342d9a083b48876ddd2b0636f0bd
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-09-06 21:28:16 +0530

    Add firebase get data functionality

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,6 @@
 import firebase from "firebase/app";
-import 'firebase/firestore'
-import 'firebase/auth'
+import 'firebase/firestore';
+import 'firebase/auth';
 
 const config = {
   apiKey: "AIzaSyCn7xcULiExzTpPfFT_ZrEPPEN74VKrcwc",
@@ -13,36 +13,38 @@ const config = {
   measurementId: "G-CV3ZRW2WNX"
 };
 
+firebase.initializeApp(config);
+
 export const createUserProfileDocument = async (userAuth, additionalData) => {
-  if (!userAuth) return
-  const userRef = firestore.doc(`users/${userAuth.uid}`)
-  const snapShot = await userRef.get()
+  if (!userAuth) return;
 
-  if(!snapShot.exists) {
-    const {displayName, email} = userAuth
-    const createdAt = new Date()
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
 
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
     try {
-      userRef.set({
+      await userRef.set({
         displayName,
         email,
         createdAt,
         ...additionalData
-      })
+      });
     } catch (error) {
-      console.log('error creating user', error.message)
+      console.log('error creating user', error.message);
     }
   }
-  return userRef
-}
 
-firebase.initializeApp(config)
+  return userRef;
+};
 
-export const auth = firebase.auth()
-export const firestore = firebase.firestore()
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
 
-const provider = new firebase.auth.GoogleAuthProvider()
-provider.setCustomParameters({prompt: 'select_account'})
-export const signInWithGoogle = () => auth.signInWithPopup(provider)
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
-export default firebase
+export default firebase;
