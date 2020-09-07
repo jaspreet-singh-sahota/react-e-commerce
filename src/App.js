7d45cb45872049c3da794e29c2b1c82ba072ed79
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-09-07 19:53:23 +0530

    Connect header with the root reducer

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="container">
-        <Header currentUser={this.state.currentUser} />
+        <Header/>
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import './header.styles.scss';
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { auth } from "../../firebase/firebase.utils";
+import { connect } from "react-redux";
 
 const Header = ({currentUser}) => (
   <div className="header">
@@ -21,4 +22,10 @@ const Header = ({currentUser}) => (
   </div>
 )
 
-export default Header
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+})
+
+console.log(connect(mapStateToProps)(Header))
+
+export default connect(mapStateToProps)(Header)
