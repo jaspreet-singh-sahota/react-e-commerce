dacda1d5532f5b6e2844b01e6f41a154d0ceba1e
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-09-11 20:37:49 +0530

    Add reselector

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,6 +3,7 @@ import CustomButton from "../custom-button/custom-button.component";
 import "./cart-dropdown.styles.scss";
 import CartItem from '../cart-item/cart-item.component'
 import { connect } from 'react-redux';
+import { selectCartItems } from '../../redux/cart/cart.selector';
 
 function CartDropdown({ cartItems }) {
   return (
@@ -19,8 +20,8 @@ function CartDropdown({ cartItems }) {
   )
 }
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems
+const mapStateToProps = state => ({
+  cartItems: selectCartItems(state)
 })
 
 export default connect(mapStateToProps)(CartDropdown)
