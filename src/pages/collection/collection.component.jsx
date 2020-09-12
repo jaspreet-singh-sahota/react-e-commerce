2862c65e4069e6bc4df247fef9dc80e9ad7ba077
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-09-12 18:40:23 +0530

    Add collection page

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import CollectionItem from '../../components/collection-item/collection-item.component'
+import './collection.styles.scss'
+
+function collectionPage() {
+  return (
+    <div className="collection-page">
+      <h2>Collection Page</h2>
+    </div>
+  )
+}
+
+export default collectionPage
+
+
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import CollectionsOverviewComponent from '../../components/collections-overview/collections-overview.component';
+import { Route } from 'react-router-dom';
+import collectionPage from '../collection/collection.component';
 
 
-const ShopPage = ({collections}) => (
+const ShopPage = (match) => (
   <div className='shop-page'>
-    <CollectionsOverviewComponent/>
+    <Route path={`${match.path}`} component={CollectionsOverviewComponent}/>
+    <Route path={`${match.path}/:collectionId`} component={collectionPage}/>
   </div>
 );
 
