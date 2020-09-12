5cf1e4bb463d9cd82cd42510efaa557a9d6e3b13
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-09-12 06:06:29 +0530

    Add selection overview

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,20 +1,12 @@
 import React from 'react';
+import CollectionsOverviewComponent from '../../components/collections-overview/collections-overview.component';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-import { selectCollections } from '../../redux/shop/shop.selector.js';
 
-const ShopPage = ({ collections }) => (
+const ShopPage = ({collections}) => (
   <div className='shop-page'>
-    {collections.map(({ id, ...otherCollectionProps }) => (
-      <CollectionPreview key={id} {...otherCollectionProps} />
-    ))}
+    <CollectionsOverviewComponent/>
   </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollections
-})
 
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
